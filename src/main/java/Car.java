517cd81f4da87f3d6778c70e28bad5684f698d90
Kostya70 <Mirror50s@mail.ru>
2023-02-28 15:48:08 +0500

    add new lesson

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,6 +1,6 @@
 import java.util.Objects;
 
-public class Car {
+public class Car implements Comparable<Car> {
     private String brand;
     private int number;
 
@@ -26,12 +26,26 @@ public class Car {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Car car = (Car) o;
-        return number == car.number && Objects.equals(brand, car.brand);
+        return number == car.number &&
+                Objects.equals(brand, car.brand);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(brand, number);
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return brand.compareTo(o.brand);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", number=" + number +
+                '}';
     }
 }
 
